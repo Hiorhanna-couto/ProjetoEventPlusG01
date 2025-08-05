afe89fa3d4abbcc41ba9fdd971b065c5f546fd30
HIORHANNA COUTO SANTANA <52267963825@info.local>
2025-08-05 16:43:18 -0300

    teste

---
+++ src/Services/services.js
@@ -0,0 +1,18 @@
+// Importa o Axios, que é uma biblioteca para fazer requisições HTTP
+import axios from "axios";
+
+// Define a porta onde a API backend está rodando localmente
+const apiPorta = "5289";
+
+// Monta a URL base da API local usando a porta definida
+const apiLocal = `http://localhost:${apiPorta}/api/`;
+const apiAzure = "https://apieventhiorhanna-gmdhffewanevasfr.brazilsouth-01.azurewebsites.net/api/"
+
+// Cria uma instância do Axios já configurada com a URL base da API
+const api = axios.create({
+    baseURL: apiAzure
+});
+
+// Exporta essa instância para ser usada em outras partes do projeto (ex: para fazer chamadas à API)
+export default api;
+
